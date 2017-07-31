f8eacc2cb680850cc1e13a714beba7a2e04655f8
jackss011 <jackgaming81@gmail.com>
2017-07-31 02:34:18 +0200

    added project components

---
+++ app/src/view/components/projects/ProjectPane.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class ProjectPane extends React.Component {
+
+}
+
+export default ProjectPane
---
+++ app/src/view/components/projects/ProjectRow.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class ProjectRow extends React.Component {
+
+}
+
+export default ProjectRow
