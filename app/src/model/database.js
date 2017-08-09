dbdb8ff9a53dc5aca9ca293d23166f8191cf92e1
jackss011 <jackgaming81@gmail.com>
2017-08-09 03:29:58 +0200

    added projects change callback

--- app/src/model/database.js
+++ app/src/model/database.js
@@ -2,12 +2,43 @@ import firebase from 'firebase'
 
 
 class Database {
-    constructor() {}
+    constructor() {
+        this._onProjects = null
+        this._uidCache = null
+    }
 
-    get uid() { return firebase.auth().currentUser && firebase.auth().currentUser.uid }
+    initialize() {
+        firebase.auth().onAuthStateChanged(user => {
+            
+            if(user) {
+                this._uidCache = user && user.uid
+
+                this._projectsListener = this.ref('projects').on('value', snapshot => {
+                    if(this.onProjects) this.onProjects(snapshot.val())
+                })
+            }
+            else if(this.uidCache) {
+                this.ref('projects').off('value', this._projectsListener)
+            }
+        })
+    }
+
+    get onProjects() { return this._onProjects }
+
+    set onProjects(callback) {
+        if(callback && typeof callback !== 'function') throw 'callback must be a function'
+        this._onProjects = callback
+    }
+
+    get uid() { return firebase.auth().currentUser.uid }
+
+    get uidCache() { return this._uidCache}
+
+    ref(path) { return firebase.database().ref(`users/${this.uidCache}/${path}`) }
 
     addProject(name, description) {
-        console.log(this.uid)
+        // firebase.database().ref(`users/${this.uid}/projects`)
+        //     .push().set({name, description})
     }
 }
 
--- app/src/redux/middleware/database-middleware.js
+++ app/src/redux/middleware/database-middleware.js
@@ -5,11 +5,14 @@ import ActionTypes from '../actions/action-types'
 export default database => {
     return ({dispatch}) => {
        
-
+        database.onProjects = projects => 
+            dispatch(ActionsGenerator.onProjects(projects))
 
         return next => action => {
             switch(action.type) {
-                
+                case ActionTypes.INITIALIZE:
+                    database.initialize()
+                    break
             }
 
             return next(action)
