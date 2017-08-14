4b8706841ae2d3d3aae2faf6ca1ed05ac5d5ef46
jackss011 <jackgaming81@gmail.com>
2017-08-14 03:55:41 +0200

    removed SoftButton file

--- app/src/view/components/SoftButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class SoftButton extends React.Component {
-    render() {
-        return <button className="soft-button" onClick={this.props.action}>{this.props.children}</button>
-    }
-}
-
-export default SoftButton //TODO remove this class
