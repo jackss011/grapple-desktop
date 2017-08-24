f7502411ead7cab5d59c3d7357615a70f3052a08
jackss011 <jackgaming81@gmail.com>
2017-08-24 03:58:25 +0200

    reworked storage_middleware
    - added shorthand functions: get, set ,remove
    - bug fix: remove selected_project from storage if the project isn't present in the project list

--- app/src/redux/middleware/storage-middleware.js
+++ app/src/redux/middleware/storage-middleware.js
@@ -3,18 +3,34 @@ import ActionTypes from '../actions/action-types'
 
 const formatKey = name => `grapple:${name}`
 
+const set = (key, value) => window.localStorage.setItem(formatKey(key), value)
+
+const get = key => window.localStorage.getItem(formatKey(key))
+
+const remove = key => window.localStorage.removeItem(formatKey(key))
+
+const Keys = {
+    SELECTED_PROJECT: 'selected-project'
+}
+
 
 export default ({dispatch}) => next => action => {
     const res = next(action)
 
     switch(action.type) {
         case ActionTypes.INITIALIZE:
-            const selected = window.localStorage.getItem(formatKey('selected-project'))
+            const selected = get(Keys.SELECTED_PROJECT)
             if(selected) dispatch(ActionsGenerator.selectProject(selected))
             break
 
         case ActionTypes.SELECT_PROJECT:
-            window.localStorage.setItem(formatKey('selected-project'), action.uid)
+            set(Keys.SELECTED_PROJECT, action.uid)
+            break
+
+        case ActionTypes.ON_PROJECTS:
+            const cursel = get(Keys.SELECTED_PROJECT)
+            if(!action.projects || !action.projects.hasOwnProperty(cursel))
+                remove(Keys.SELECTED_PROJECT)
             break
     }
 
