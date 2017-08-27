cd7b82725149d116eaf218dd85836ef60ccd24d7
jackss011 <jackgaming81@gmail.com>
2017-08-27 05:20:58 +0200

    moved delete button to left in project_details

--- app/src/view/components/projects/details/ProjectDetails.jsx
+++ app/src/view/components/projects/details/ProjectDetails.jsx
@@ -11,14 +11,19 @@ class ProjectDetails extends React.Component {
 
         return (
             <div className="project-details">
-                <div className="name">{this.props.project.name}</div>
-                <div className="description">{this.props.project.description}</div>
-                <button 
-                    className="delete-button" 
-                    onClick={() => this.props.onDelete(this.props.projectUid)}
-                >
-                    <i className="material-icons small">clear</i>
-                </button>
+                <div className="texts">
+                    <div className="name">{this.props.project.name}</div>
+                    <div className="description">{this.props.project.description}</div>
+                </div>
+
+                <div className="action-bar">
+                    <button
+                        className="delete-button"
+                        onClick={() => this.props.onDelete(this.props.projectUid)}
+                    >
+                        <i className="material-icons small">clear</i>
+                    </button>
+                </div>
             </div>
         )
     }
